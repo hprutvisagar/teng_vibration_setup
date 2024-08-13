d0aae280d27b692bf4381ecf361c137ed633ecf8
Sagar HP <68324225+hprutvisagar@users.noreply.github.com>
2024-08-13 17:06:41 +0200

    .

--- oscilloscope_test.py
+++ oscilloscope_test.py
@@ -32,74 +32,73 @@ from time import time
 
 
 def main():
-    # Make sure you have the last version of the RsInstrument
-    RsInstrument.assert_minimum_version('1.53.0')
-    # mxo = None
-    try:
-        # Adjust the VISA Resource string to fit your instrument
-        mxo = RsInstrument('USB0::0x0AAD::0x01D6::111986::INSTR', id_query=True, reset=True, options="SelectVisa='rs'")
-        mxo.logger.mode = LoggingMode.On
-        mxo.visa_timeout = 6000  # Timeout for VISA Read Operations
-        mxo.opc_timeout = 3000  # Timeout for opc-synchronised operations
-        mxo.instrument_status_checking = True  # Error check after each command
-    except Exception as ex:
-        print('Error initializing the instrument session:\n' + ex.args[0])
-        exit()
+    import RsInstrument
+    import numpy as np
+    import csv
+    import matplotlib.pyplot as plt
+    import pandas as pd
 
-    print(f'Device IDN: {mxo.idn_string}')
-    print(f'Device Options: {",".join(mxo.instrument_options)}\n')
+    # Connect to the oscilloscope
+    rtb = RsInstrument.RsInstrument('USB0::0x0AAD::0x01D6::111986::INSTR', True, True)
 
-    # Basic settings - To test with mxo probe compensation signal connected to CH1
-    mxo.write('SYSTem:DISPlay:UPDate ON')  # Keep display on while under remote control
-    mxo.write_with_opc('AUToscale', 10000)  # Perform autoscaling on the used signal, timeout 10 seconds
-    mxo.write('TRIGger:MODE NORMal')  # Set trigger to Normal to avoid unwanted triggering while in Auto mode
+    # -----------------------------------------------------------
+    # Basic Settings:
+    # ---------------------------- -------------------------------
+    rtb.write_str("TIM:ACQT 120e-3")  # Horizontal range---1.2ms Acquisition time
+    rtb.write_str("CHAN1:RANG 8")  # vertical range 0.6V (0.5V/div)
+    rtb.write_str("CHAN1:OFFS 0.0")  # Offset 0
+    rtb.write_str("CHAN1:COUP ACL")  # Coupling AC 1MOhm
+    rtb.write_str("CHAN1:STAT ON")  # Switch Channel 1 ON
 
-    # Perform acquisition
-    start = time()
-    mxo.write_str_with_opc("RUNsingle")  # Single acquisition is presupposition for retrieving waveform data
-    stop = time()
-    print('MXO triggered, capturing data ...')
-    print(f'Number of sample points: {mxo.query_float("ACQ:POIN?")}')
-    print(f'Data capturing elapsed time: {stop - start:.3f}sec\n')
+    # -----------------------------------------------------------
+    # Trigger Settings:
+    # -----------------------------------------------------------
+    rtb.write_str("TRIG:A:MODE AUTO")  # Trigger Auto mode in case of no signal is applied
+    rtb.write_str("TRIG:A:TYPE EDGE;:TRIG:A:EDGE:SLOP POS")  # Trigger type Edge Positive
+    rtb.write_str("TRIG:A:SOUR CH1")  # Trigger source CH1
+    rtb.write_str("TRIG:A:LEV1 0.0")  # Trigger level 0.05V
+    rtb.query_opc()  # Using *OPC? query waits until all the instrument settings are finished
 
-    # Get and plot binary data
-    start = time()
-    mxo.write_str("FORMat:DATA REAL,32;:FORMat:BORDer LSBFirst")
-    mxo.bin_float_numbers_format = BinFloatFormat.Single_4bytes
-    mxo.data_chunk_size = 100000  # transfer in blocks of 100k bytes (default)
-    print('Now start to transfer binary waveform data. Please wait for about 20 seconds...')
-    data_bin = mxo.query_bin_or_ascii_float_list("CHAN:DATA?")
+    # Initiate a single acquisition and wait for it to finish
+    rtb.write_str_with_opc("SINGle", 3000)
 
-    print(f'Binary waveform transfer elapsed time: {time() - start:.3f}sec\n')
+    # Query array of floats in ASCII format
+    waveform = rtb.query_bin_or_ascii_float_list('FORM ASC;:CHAN1:DATA?')
 
-    plt.figure(1)
-    plt.plot(data_bin)
-    plt.title('Binary waveform')
+    # Fetch timebase settings
+    x_increment = float(rtb.query('CHAN1:DATA:XINC?'))  # Horizontal scale
+    x_origin = float(rtb.query('CHAN1:DATA:XOR?'))  # Horizontal position
 
-    # Get and plot ASCII data just to show how long it would take in comparison
-    start = time()
-    mxo.write_str("FORM:DATA ASC")
-    mxo.data_chunk_size = 100000  # transfer in blocks of 100k bytes (default)
-    print('Now start to transfer ASCII waveform data. Please wait for about 120 seconds...')
-    data_asc = mxo.query_bin_or_ascii_float_list("CHAN:DATA?")
-    print(f'ASCII waveform transfer elapsed time: {time() - start:.3f}sec')
-    plt.figure(2)
-    plt.plot(data_asc)
-    plt.title('ASCII waveform')
+    # Calculate time data
+    time_data = np.arange(0, len(waveform)) * x_increment + x_origin
 
-    # get screenshot
-    file_path_instr = r'/home/instrument/userData/Device_Screenshot.png'  # MXO is a LINUX based device
-    file_path_pc = r'c:\temp\Device_Screenshot.png'  # While this path is based on the WIN world
 
-    mxo.write_str("HCOPy:DEVice1:LANGuage PNG")
-    mxo.write_str(f"MMEMory:NAME '{file_path_instr}'")
-    mxo.write_str_with_opc("HCOPy:IMMediate1")
-    mxo.read_file_from_instrument_to_pc(file_path_instr, file_path_pc)
-    print(f'\nTransferred screen shot to {file_path_pc}\n')
+    rtb.close()
 
-    mxo.close()
-    print('Close the plot windows to end the script...')
+    # data plotting
+    data = pd.DataFrame({
+        'time_data': time_data,
+        'waveform': waveform
+    })
+
+    data.plot(x='time_data', 
+              y='waveform', 
+              xlabel='Time', 
+              ylabel='Voltage (V)', 
+              title='Oscilloscope Waveform', 
+              figsize=(16,8),
+              grid=True
+              )
     plt.show()
+
+    # Save waveform data to CSV file
+    csv_file_path = 'waveform_data.csv'
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Time (s)', 'Voltage (V)'])  # Write the header
+        csv_writer.writerows(zip(time_data, waveform))  # Write the time and voltage data
+
+        print(f"Waveform data saved to '{csv_file_path}'")
 
 
 if __name__ == "__main__":
